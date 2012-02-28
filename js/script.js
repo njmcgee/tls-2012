5d6d879f8d9571aa7bf898e334d0f23e714acadf
Nathan McGinness <nathanmcginness@gmail.com>
2012-02-27 16:42:15 -0800

    all the basics in place

--- js/script.js
+++ js/script.js
@@ -3,6 +3,16 @@
 */
 
 
+$(document).ready(function () {		
+	$("a[rel=image-gallery]").fancybox({
+		'transitionIn'		: 'none',
+		'transitionOut'		: 'none',
+		'titlePosition' 	: 'over',
+		'titleFormat'		: function(title, currentArray, currentIndex, currentOpts) {
+			return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+		}
+	});
+});
 
 
 
